d75d661c8759d9a84b26839d51a6cba0eef2d232
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2024-09-17 17:59:41 +0000

    changes

--- app/config.py
+++ app/config.py
@@ -1,13 +1,12 @@
 import os
 
-db_host = os.environ.get("DB_ADDRESS", "db") 
-db_name = os.environ.get("DB_NAME", "mydb")
+db_host = os.environ.get("DB_ADDRESS") 
+db_name = os.environ.get("DB_NAME")
 db_port = "5432"
-postgres_username = os.environ.get("POSTGRES_USERNAME", "postgres")
-postgres_password = os.environ.get("POSTGRES_PASSWORD", "password")
+postgres_username = os.environ.get("POSTGRES_USERNAME")
+postgres_password = os.environ.get("POSTGRES_PASSWORD")
 
 class Config:
     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://{postgres_username}:{postgres_password}@{db_host}:{db_port}/{db_name}')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@db:5432/mydb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tyVs6cQBoz@dev-web-app-db.cfykukwcw419.ap-south-1.rds.amazonaws.com:5432/mydb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
