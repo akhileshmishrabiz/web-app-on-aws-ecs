ca93a3693cff2c524d12b1f7dcd71be2a5489eaf
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2024-09-23 01:32:29 +0530

    hardcoding the creds to test

--- app/config.py
+++ app/config.py
@@ -7,8 +7,8 @@ postgres_username = os.environ.get("POSTGRES_USERNAME")
 postgres_password = os.environ.get("POSTGRES_PASSWORD")
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://{postgres_username}:{postgres_password}@{db_host}:{db_port}/{db_name}')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tyVs6cQBoz@dev-web-app-db.cfykukwcw419.ap-south-1.rds.amazonaws.com:5432/mydb'
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://{postgres_username}:{postgres_password}@{db_host}:{db_port}/{db_name}')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tyVs6cQBoz@dev-app-db.cfykukwcw419.ap-south-1.rds.amazonaws.com:5432/mydb'
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@db:5432/mydb'   # with docker-compose
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
