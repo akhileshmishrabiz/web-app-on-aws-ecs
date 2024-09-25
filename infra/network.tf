34b84487da3890f168d7771520558d4f07bef78b
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2024-09-25 13:33:20 +0530

    betwork changesnetwork

--- infra/network.tf
+++ infra/network.tf
@@ -51,28 +51,28 @@ resource "aws_route" "internet_access" {
   gateway_id             = aws_internet_gateway.gateway.id
 }
 
-# resource "aws_eip" "gateway" {
-#   count      = 2
-#   depends_on = [aws_internet_gateway.gateway]
-# }
+resource "aws_eip" "gateway" {
+  count      = 2
+  depends_on = [aws_internet_gateway.gateway]
+}
 
-# resource "aws_nat_gateway" "gateway" {
-#   count         = 2
-#   subnet_id     = element(aws_subnet.public.*.id, count.index)
-#   allocation_id = element(aws_eip.gateway.*.id, count.index)
-# }
+resource "aws_nat_gateway" "gateway" {
+  count         = 2
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  allocation_id = element(aws_eip.gateway.*.id, count.index)
+}
 
-# resource "aws_route_table" "private" {
-#   count  = 2
-#   vpc_id = aws_vpc.main.id
-#   route {
-#     cidr_block     = "0.0.0.0/0"
-#     nat_gateway_id = element(aws_nat_gateway.gateway.*.id, count.index)
-#   }
-#   tags = {
-#     Name = "ECS EC2 Private Route Table ${count.index}"
-#   }
-# }
+resource "aws_route_table" "private" {
+  count  = 2
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = element(aws_nat_gateway.gateway.*.id, count.index)
+  }
+  tags = {
+    Name = "ECS EC2 Private Route Table ${count.index}"
+  }
+}
 
 resource "aws_route_table_association" "private" {
   count          = 2
