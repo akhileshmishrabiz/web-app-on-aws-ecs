817b67f66bcfdd5064e374cc8be5e15be5e23e23
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2024-09-29 20:05:18 +0000

    test

--- app/config.py
+++ app/config.py
@@ -7,8 +7,8 @@ postgres_username = os.environ.get("POSTGRES_USERNAME")
 postgres_password = os.environ.get("POSTGRES_PASSWORD")
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://{postgres_username}:{postgres_password}@{db_host}:{db_port}/{db_name}')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tyVs6cQBoz@dev-app-db.cfykukwcw419.ap-south-1.rds.amazonaws.com:5432/mydb'
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://{postgres_username}:{postgres_password}@{db_host}:{db_port}/{db_name}')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tyVs6cQBoz@dev-app-db.cfykukwcw419.ap-south-1.rds.amazonaws.com:5432/mydb'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_BROKER_URL = 'redis://redis:6379/0'
