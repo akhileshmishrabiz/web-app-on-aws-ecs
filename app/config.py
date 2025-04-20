92f62c9ff363fc0b1a4dba476c38d4df726ced0c
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2025-04-20 10:09:57 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -10,14 +10,15 @@ class Config:
 
     # Uncomment when running on prod
     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI =  f'postgresql://{postgres_username}:{postgres_password}@{db_host}:{db_port}/{db_name}'
-    #### SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tyVs6cQBoz@dev-app-db.cfykukwcw419.ap-south-1.rds.amazonaws.com:5432/mydb'
 
+    # with docker-compose
+    # comment out when running with docker compose
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@db:5432/mydb' 
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_BROKER_URL = 'redis://redis:6379/0'
     CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
-     # with docker-compose
-     # comment out when running with docker compose
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@db:5432/mydb' 
+  
 
     
